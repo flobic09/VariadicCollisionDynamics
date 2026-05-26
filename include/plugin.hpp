aa8b1553136ccb2040e4c7c208cdbadaff936871
LG <legendman_2009@live.com>
2026-05-27 00:23:40 +0200

    preparing template

---
+++ include/plugin.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <RE/Skyrim.h>
+#include <RE/V/VirtualMachine.h>
+#include <SKSE/SKSE.h>
+#include <REL/Relocation.h>
+
+using namespace std::literals;
+using namespace std;
+
+#define RELOCATION_OFFSET(SE, AE) REL::VariantOffset(SE, AE, 0).offset()
+
---
+++ src/plugin.cpp
@@ -0,0 +1,44 @@
+﻿
+#include "plugin.hpp"
+#include "logger.hpp"
+
+static void MessageHandler(SKSE::MessagingInterface::Message* msg) {
+    switch (msg->type) {
+    case SKSE::MessagingInterface::kPostLoad:
+    {
+        break;
+    }
+    case SKSE::MessagingInterface::kSaveGame:
+    {
+        break;
+    }
+    case SKSE::MessagingInterface::kPreLoadGame:
+    {
+        break;
+    }
+    case SKSE::MessagingInterface::kPostLoadGame:
+    {
+        break;
+    }
+    case SKSE::MessagingInterface::kNewGame:
+    {
+   
+        break;
+    }
+    case SKSE::MessagingInterface::kDataLoaded:
+    {
+     
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+SKSEPluginLoad(const SKSE::LoadInterface* skse) {
+    SKSE::Init(skse);
+    setupLog(spdlog::level::info);
+    logger::info("Variadic Collision Dynamics Plugin is Loaded");
+    SKSE::GetMessagingInterface()->RegisterListener(MessageHandler);
+    return true;
+}
